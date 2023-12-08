300e73de107e3ba6eb749af548cee555200481a7
Federico <nvfede@gmail.com>
2023-12-08 18:04:22 +0100

    login styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,6 +22,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const router = useRouter();
   const [rememberMe, setRememberMe] = useState(false);
+
   const handleLogin = async () => {
     const success = await signIn(email, password);
     if (success) {
@@ -39,43 +40,52 @@ const Login = () => {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center gap-10">
       <AuthCard>
-          <input
-            className="p-5 m-5"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-          />
-          <input
-            className="p-5 m-5"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
+        <input
+          className="rounded-md shadow-sm w-full block mt-2 p-2 border border-gray-200 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          className="rounded-md shadow-sm w-full block mt-2 p-2 border border-gray-200 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
 
-          <input
-            type="checkbox"
-            checked={rememberMe}
-            onChange={(e) => setRememberMe(e.target.checked)}
-          />
-          <label htmlFor="rememberMe">Recuérdame</label>
-
-          <button onClick={handleLogin} className="bg-red-500 p-5 text-white">
-            Submit
-          </button>
-          
-
-          <div className="flex">
-            Do you have an account? <Link href={`/signup`}>Sign Up</Link>
+        <div className="flex justify-between my-4  border-b border-b-gray-200 pb-4">
+          <div className="flex gap-2 items-center">
+            <input
+              type="checkbox"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label htmlFor="rememberMe ml-2">Recuérdame</label>
           </div>
-          {/* Login form elements */}
-          <button className="bg-red-400 p-5" onClick={handleGoogleLogin}>
-            Login With Google
+
+          <button
+            onClick={handleLogin}
+            className="bg-red-500 py-2 px-4 rounded-lg text-white"
+          >
+            Send
           </button>
+        </div>
 
-          {state.error && <p className="text-red-500">Error: {state.error}</p>}
+        {/* Login form elements */}
+        <button
+          className="bg-[#EA4335] w-full px-4 py-2 rounded-lg text-white"
+          onClick={handleGoogleLogin}
+        >
+          Login With Google
+        </button>
+        <div className="flex my-2 gap-4">
+          Do you have an account? <Link href={`/signup`} >Sign Up</Link>
+        </div>
+        <div className="flex h-10 ">
+          {state.error && <p className="text-red-500 ">Error: {state.error}</p>}
+        </div>
 
-          <Link href="/password-reset">¿Olvidaste tu contraseña?</Link>
-        
+        <Link href="/password-reset">¿Olvidaste tu contraseña?</Link>
       </AuthCard>
     </div>
   );
